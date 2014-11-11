dd8d618a0c3dd2d1ed27ac5e1268abde2ef6c3b6
Ryan Fairchild <rfairchild@glgroup.com>
2014-11-10 22:22:02 -0500

    whitespace

--- async.js
+++ async.js
@@ -4,38 +4,38 @@ var request = require('request');
 console.log('ex3. async running');
 
 async.waterfall([
-	function(cb) {
-		request({ url: 'http://reqr.es/api/users', json:true }, function(err, res, body) {			
-			cb(err, body);
-		})
-	},
-	function(users, cb) {
-		try {			
-			var urls = users.data.map(function(user) {			
-				return 'http://reqr.es/api/user/' + user.id;
-			});
-			async.map(urls, request, function(err, responses) {							
-				var results = responses.map(function(response) {					
-					try {
-						var body = JSON.parse(response.body);												
-						// throw new Error('oh no');						
-						return body.data.id;
-					} catch(e) {
-						cb(e, null);
-						return false;
-					}					
-				})
-				cb(null, results);		
-			})	
-		} catch (e) {
-			cb(e, null);
-		}	
-	}
+  function(cb) {
+    request({ url: 'http://reqr.es/api/users', json:true }, function(err, res, body) {
+      cb(err, body);
+    })
+  },
+  function(users, cb) {
+    try {
+      var urls = users.data.map(function(user) {
+        return 'http://reqr.es/api/user/' + user.id;
+      });
+      async.map(urls, request, function(err, responses) {
+        var results = responses.map(function(response) {
+          try {
+            var body = JSON.parse(response.body);
+            // throw new Error('oh no');
+            return body.data.id;
+          } catch(e) {
+            cb(e, null);
+            return false;
+          }
+        })
+        cb(null, results);
+      })
+    } catch (e) {
+      cb(e, null);
+    }
+  }
 ], function(err, results) {
-	if (err) {
-		console.log(err.stack);
-	} else {
-		console.log(results);
-		console.log('done\n');
-	}
+  if (err) {
+    console.log(err.stack);
+  } else {
+    console.log(results);
+    console.log('done\n');
+  }
 })
--- pyramid.js
+++ pyramid.js
@@ -2,38 +2,38 @@ var request = require('request');
 
 console.log('ex1. pyramid running');
 request({ url: 'http://reqr.es/api/users', json:true }, function(err, results, body) {
-	if(err) {
-		console.log(err);
-	} else {
+  if(err) {
+    console.log(err);
+  } else {
 
-		var users = body.data;
+    var users = body.data;
 
-		if(!users) {
-			cb('users bad', null);
-		}
+    if(!users) {
+      cb('users bad', null);
+    }
 
-		var userLength = users.length;
-		var count = 0;
-		var results = [];
+    var userLength = users.length;
+    var count = 0;
+    var results = [];
 
-		users.forEach(function(user) {
-			//throw new Error('im a bad exception');
+    users.forEach(function(user) {
+      //throw new Error('im a bad exception');
 
-			request({ url: "http://reqr.es/api/user/" + user.id, json:true }, function(err, request, body) {
-				if(err) {
-					console.log('fetching user failed');
-				} else {
+      request({ url: "http://reqr.es/api/user/" + user.id, json:true }, function(err, request, body) {
+        if(err) {
+          console.log('fetching user failed');
+        } else {
 
-					results.push(body.data.id);
-					count++;
-			
-					if (count == userLength) {
-						console.log(results);
-						console.log('done\n');
-					}
-				}
-			});
-			
-		});
-	}
+          results.push(body.data.id);
+          count++;
+
+          if (count == userLength) {
+            console.log(results);
+            console.log('done\n');
+          }
+        }
+      });
+
+    });
+  }
 })
